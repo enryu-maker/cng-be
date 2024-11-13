a1e7effed0fe4d14df9234e6662e1f16ef413f8e
enryu-maker <akifkhan60067@gmail.com>
2024-11-14 00:16:14 +0530

    DB ISSUE

--- app/database.py
+++ app/database.py
@@ -2,9 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-PROD_URL = "postgresql://cngstation_user:OpINFXni1oT9BInwGTmvyhnKlMetBqjQ@dpg-cspql72j1k6c739ppm7g-a.oregon-postgres.render.com/cngstation"
-SQL_DATABASE_URL = "postgresql://postgres:123456@localhost/smartcng"
-engine = create_engine(PROD_URL)
+# PROD_URL = "postgresql://cngstation_user:OpINFXni1oT9BInwGTmvyhnKlMetBqjQ@dpg-cspql72j1k6c739ppm7g-a.oregon-postgres.render.com/cngstation"
+SQL_DATABASE_URL = "sqlite:///./smartcng.db"
+PSQL_DATABASE_URL = "postgresql://postgres:123456@localhost/smartcng"
+engine = create_engine(SQL_DATABASE_URL)
 
 SessionLocale = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
